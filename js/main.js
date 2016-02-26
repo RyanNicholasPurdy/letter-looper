98cad6d2f7c39cd6bca608b7a06fe13a33511b4f
Ryan Purdy <Ryanpurdy@Ryans-MacBook-Pro-2.local>
2016-02-26 10:18:02 -0500

    Commit!

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var newLetter = prompt("Enter a letter!");
+
+// The `i` variable is a counter, to count how many times the loop has occurred
+var i;
+
+// Let’s create a loop that repeats 10 times
+for (i = 0; i < 10; i++) {
+  // The code in here will be executed 10 times
+  document.write(newLetter + "<br>");
+}
